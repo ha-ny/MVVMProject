5aa603090b95066301eba63ed0fcc1b58f36b0d8
ha-ny <hany01.dev@gmail.com>
2023-09-19 00:48:14 +0900

    resultLabel 위치 변경

--- MVVMProject/Login/LoginView.swift
+++ MVVMProject/Login/LoginView.swift
@@ -10,6 +10,12 @@ import SnapKit
 
 class LoginView: BaseView {
 
+    let resultLabel = {
+        let view = UILabel()
+        view.textAlignment = .center
+        return view
+    }()
+    
     let emailTextField = {
         let view = UITextField()
         view.placeholder = "이메일 주소"
@@ -63,26 +69,25 @@ class LoginView: BaseView {
         view.layer.borderColor = UIColor.black.cgColor
         return view
     }()
-    
-    let resultLabel = {
-        let view = UILabel()
-        view.textAlignment = .center
-        return view
-    }()
-    
+        
     override func setConfigure() {
+        addSubview(resultLabel)
         addSubview(emailTextField)
         addSubview(passwordTextField)
         addSubview(nicknameTextField)
         addSubview(codeTextField)
         addSubview(signUpButton)
-        addSubview(resultLabel)
     }
 
     override func setContains() {
         
+        resultLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.safeAreaLayoutGuide).offset(80)
+            make.centerX.equalToSuperview()
+        }
+        
         emailTextField.snp.makeConstraints { make in
-            make.top.equalTo(self.safeAreaLayoutGuide).offset(85)
+            make.top.equalTo(resultLabel.snp.bottom).offset(30)
             make.size.equalTo(CGSize(width: 350, height: 40))
             make.centerX.equalToSuperview()
         }
@@ -108,12 +113,6 @@ class LoginView: BaseView {
         signUpButton.snp.makeConstraints { make in
             make.top.equalTo(codeTextField.snp.bottom).offset(30)
             make.size.equalTo(CGSize(width: 350, height: 50))
-            make.centerX.equalToSuperview()
-        }
-        
-        resultLabel.snp.makeConstraints { make in
-            make.top.equalTo(signUpButton.snp.bottom).offset(30)
-            make.size.equalTo(350)
             make.centerX.equalToSuperview()
         }
     }
